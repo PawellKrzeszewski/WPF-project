1e683d8fa768987f2a758ba4e15aa24185a72324
Paweł Krzeszewski <pkrzeszewski@s201.local>
2024-10-28 08:15:06 +0100

    add mouseEnter and mouseLeave event on time button

--- wpf project/wpf project/MainWindow.xaml.cs
+++ wpf project/wpf project/MainWindow.xaml.cs
@@ -25,5 +25,16 @@ namespace wpf_project
         {
             MessageBox.Show("Witaj swiecie");
         }
+
+        private void btnTime_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DateTime data = DateTime.Now;
+            btnTime.Content = data.ToString("T");
+        }
+
+        private void btnTime_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnTime.Content = "Czas";
+        }
     }
 }
